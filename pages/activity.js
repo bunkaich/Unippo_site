6081277089e80790bfc585ee6c832a899306f921
Kohei Yamaguchi <bunkaich@gmail.com>
2023-03-12 21:25:54 +0900

    change activity text

--- pages/activity.js
+++ pages/activity.js
@@ -137,10 +137,7 @@ export default function Activity() {
             <div>
               フリースクール概要
               <br />
-              ・日時：月二回
-              <span style={{ fontSize: 'calc(var(--body)*0.7)' }}>
-                (各月の日程詳細は下記リンク記載)
-              </span>
+              ・日時：各月の日程詳細は下記リンク記載
               <br />
               ・内容：午前中プログラム/午後自由時間
               <br />
