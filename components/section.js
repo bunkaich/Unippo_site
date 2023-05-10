7f18ffc4022786687dfc628b56ba624498c1407d
Kohei Yamaguchi <bunkaich@gmail.com>
2023-05-10 22:55:43 +0900

    edit support.js

--- components/section.js
+++ components/section.js
@@ -1,11 +1,19 @@
 import styles from 'styles/section.module.css'
 
-export default function Section({ children, fullWidth = false, color = 'yellow' }) {
+export default function Section({
+  children,
+  fullWidth = false,
+  color = 'yellow',
+  width = '800px',
+}) {
   return (
-    <section className={ fullWidth ? `${styles.fullwidth} ${styles[color]}` : styles.basic}>
-      <div className={styles.inner}>
-        {children}
-      </div>
+    <section
+      className={
+        fullWidth ? `${styles.fullwidth} ${styles[color]}` : styles.basic
+      }
+      style={{ maxWidth: width }}
+    >
+      <div className={styles.inner}>{children}</div>
     </section>
   )
 }
